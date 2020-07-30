fae859709d65ea6e3ada1b17adf792b0fc737704
Chris is Awesome <chrisramer.97@gmail.com>
2020-07-30 14:33:21 -0700

    Capitalize & reverse first & last chars

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
-	prompt("Enter a sentence");
+
+	let baseSentence = prompt("Enter a sentence");
+	console.log("Saved sentence: `" + baseSentence + "`!");
+
+	const capitalizeChars = function(sentence) {
+		const charFirst = sentence.charAt(0).toUpperCase();
+		const charLast = sentence.charAt(sentence.length - 1).toUpperCase();
+		return charFirst + sentence.slice(1, sentence.length - 1) + charLast;
+	}
+
+	const reverseChars = function(sentence) {
+		const charFirst = sentence.charAt(sentence.length - 1);
+		const charLast = sentence.charAt(0);
+		return charFirst + sentence.slice(1, sentence.length - 1) + charLast;
+	}
+
+	baseSentence = capitalizeChars(baseSentence);
+	alert("First & last letters were capitalized!\n" + baseSentence);
+
+	baseSentence = reverseChars(baseSentence);
+	alert("First & last letters were reversed!\n" + baseSentence);
+
 });
